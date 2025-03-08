e74546b2f36fef68269de8b55749c41824c808d1
mikecarter216 <michaelakpan216@gmail.com>
2025-03-08 20:28:52 +0100

    g

---
+++ js/form.js
@@ -0,0 +1,46 @@
+ document.addEventListener("DOMContentLoaded", function () {
+    const productSelect = document.getElementById("product-name");
+    const products = [
+        {
+          id: "fc-1888",
+          name: "flux capacitor",
+          averagerating: 4.5
+        },
+        {
+          id: "fc-2050",
+          name: "power laces",
+          averagerating: 4.7
+        },
+        {
+          id: "fs-1987",
+          name: "time circuits",
+          averagerating: 3.5
+        },
+        {
+          id: "ac-2000",
+          name: "low voltage reactor",
+          averagerating: 3.9
+        },
+        {
+          id: "jj-1969",
+          name: "warp equalizer",
+          averagerating: 5.0
+        }
+      ];
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+
+    // Review Counter
+    if (localStorage.getItem("reviewCount") === null) {
+        localStorage.setItem("reviewCount", "0");
+    }
+
+    document.getElementById("review-form").addEventListener("submit", function () {
+        let count = parseInt(localStorage.getItem("reviewCount")) + 1;
+        localStorage.setItem("reviewCount", count);
+    });
+});
